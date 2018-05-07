6436711f21ee8dedd51800be08486f30b6e46059
rohit <sachan.rohit@gmail.com>
2018-05-08 05:27:17 +0800

    buy sell profit

--- algosjava/src/main/java/MaxProfit.java
+++ algosjava/src/main/java/MaxProfit.java
@@ -2,11 +2,10 @@
  * Created by Rohit on 5/29/2016.
  */
 public class MaxProfit {
-/*
     public static int maxProfit(int[] prices) {
         int profit =0;
         int last_buy = Integer.MAX_VALUE;
-        int last_sell = Integer.MIN_VALUE;
+        int last_sell;
         for(int p : prices){
             if(p<last_buy){
                 last_buy = p;
@@ -17,35 +16,15 @@ public class MaxProfit {
                 }
             }
         }
-
         return profit;
 
-    }*/
-
-
+    }
 
     public static void main(String[] args) {
         int price[] = {100, 180, 260, 310, 40, 535, 695};
-        System.out.println(maxProfit2(price));
+        System.out.println(maxProfit(price));
 
     }
 
-    private static int maxProfit2(int[] prices) {
-        int profit =0;
-        int last_buy = Integer.MAX_VALUE;
-        int last_sell = Integer.MIN_VALUE;
-        for ( int price : prices){
-            if(price < last_buy){
-                last_buy = price;
-            }else{
-                last_sell = price;
-                if(last_sell- last_buy > profit){
-                    profit = last_sell - last_buy;
-                }
-            }
-
-        }
-        return profit;
-    }
 
 }
