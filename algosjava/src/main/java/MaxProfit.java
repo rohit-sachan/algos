b49f6d968016c4d4cc2ca9592e62110ff5b90ef4
rohit <sachan.rohit@gmail.com>
2018-05-20 21:23:01 +0800

    some improvements

--- algosjava/src/main/java/MaxProfit.java
+++ algosjava/src/main/java/MaxProfit.java
@@ -5,7 +5,9 @@ public class MaxProfit {
     public static int maxProfit(int[] prices) {
         int profit =0;
         int last_buy = Integer.MAX_VALUE;
-        int last_sell;
+        int mxp_buy = Integer.MAX_VALUE;
+        int last_sell = Integer.MIN_VALUE;
+        int mxp_sell = Integer.MIN_VALUE;
         for(int p : prices){
             if(p<last_buy){
                 last_buy = p;
@@ -13,17 +15,19 @@ public class MaxProfit {
                 last_sell = p;
                 if((last_sell-last_buy) > profit){
                     profit = last_sell-last_buy;
+                    mxp_sell=last_sell;
+                    mxp_buy=last_buy;
                 }
             }
         }
+        System.out.printf("buy %d sell %d\n",mxp_buy, mxp_sell);
         return profit;
 
     }
 
     public static void main(String[] args) {
-        int price[] = {100, 180, 260, 310, 40, 535, 695};
+        int price[] = {100, 180, 260, 310, 40, 535, 695, 30, 95};
         System.out.println(maxProfit(price));
-
     }
 
 
---
+++ algosjava/src/main/java/NumberOfIslands.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ 11110
+ 11010
+ 11000
+ 00000
+ ans: 1
+
+ 
+ */
+public class NumberOfIslands {
+
+
+
+}
